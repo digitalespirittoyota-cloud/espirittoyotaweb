835491a2037f490f4b862b48cca4fe168c05cf69
Tanushree Mahato <tshree.m1998@gmail.com>
2026-04-18 17:26:09 +0530

    push role

--- src/app/lib/checkAdmin.ts
+++ src/app/lib/checkAdmin.ts
@@ -1,5 +1,5 @@
-import connectDB from './src/app/lib/db';
-import Admin from './src/models/Admin';
+import connectDB from '../lib/db';
+import Admin from '../../models/Admin';
 
 async function checkAdmin() {
     try {
